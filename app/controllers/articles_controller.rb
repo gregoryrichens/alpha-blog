d35acdce72ac2f88e9b16370587e69ed063200d5
Gregory Richens <gregory@aumni.fund>
2021-06-14 13:54:53 -0600

    Add redirect to show after article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article]
+    # can't just directly use params to save to database - instead we require :article
+    # and then we permit certain attributes for that param
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      #redirect to that article's show page
+      redirect_to article_path(@article.id)
+      # could also just redirect to @article
+    else
+      render 'new'
+    end 
   end
 end
