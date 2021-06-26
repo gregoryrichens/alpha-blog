c7c75a831f35aa0b6271e64f899e990291dc2091
Gregory Richens <gregory@aumni.fund>
2021-06-26 13:20:10 -0600

    Add Edit/Update Routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,26 @@ class ArticlesController < ApplicationController
     # and then we permit certain attributes for that param
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
+      flash[:notice] = "Article was created successfully."
       #redirect to that article's show page
       redirect_to article_path(@article.id)
       # could also just redirect to @article
     else
       render 'new'
     end 
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = " Article was updated successfully."
+    else
+      render 'edit'
+    end
+    byebug
   end
 end
