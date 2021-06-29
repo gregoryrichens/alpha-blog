1a572db7ca6021d01e5602b61d95d77c38c97d8d
Gregory Richens <gregory@aumni.fund>
2021-06-29 10:27:46 -0600

    Add Navigation Links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+  
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -26,11 +27,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
       flash[:notice] = " Article was updated successfully."
       redirect_to(articles_path)
@@ -40,8 +39,13 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
